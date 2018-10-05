9fac89414929948345dcc0a23ec41aaa2b88be65
Akira Moroo <retrage01@gmail.com>
2018-10-05 17:58:54 +0900

    Clean up exec

--- exec.c
+++ exec.c
@@ -1,5 +1,8 @@
 #include "ebcvm.h"
 
+#define ARITH_OP(NAME, OP) \
+  static uint64_t NAME(uint64_t op1, uint64_t op2) { return op1 OP op2; }
+
 typedef struct op32 {
   uint32_t op1;
   uint32_t op2;
@@ -14,21 +17,41 @@ typedef struct op64 {
   uint64_t op2val;
 } op64;
 
+typedef uint64_t (*arith_op)(uint64_t, uint64_t);
+
 static op32 *read_op32(vm *, inst *);
 static op64 *read_op64(vm *, inst *);
-static vm *exec_add(vm *, inst *);
-static vm *exec_sub(vm *, inst *);
-static vm *exec_mul(vm *, inst *);
-static vm *exec_div(vm *, inst *);
-static vm *exec_mod(vm *, inst *);
-static vm *exec_and(vm *, inst *);
-static vm *exec_or(vm *, inst *);
-static vm *exec_xor(vm *, inst *);
-static vm *exec_shl(vm *, inst *);
-static vm *exec_shr(vm *, inst *);
-static vm *exec_neg(vm *, inst *);
-static vm *exec_not(vm *, inst *);
+static vm *exec_arith(vm *, inst *, uint64_t (*func)(uint64_t, uint64_t));
 static vm *exec_nop(vm *, inst *);
+
+ARITH_OP(_add, +)
+ARITH_OP(_sub, -)
+ARITH_OP(_mul, *)
+ARITH_OP(_div, /)
+ARITH_OP(_mod, %)
+ARITH_OP(_and, &)
+ARITH_OP(_or, |)
+ARITH_OP(_xor, ^)
+ARITH_OP(_shl, <<)
+ARITH_OP(_shr, >>)
+static uint64_t _neg(uint64_t op1, uint64_t op2) { return -1 * op2; }
+static uint64_t _not(uint64_t op1, uint64_t op2) { return ~op2; }
+
+arith_op arith_ops[] = {
+  NULL,
+  _add,
+  _sub,
+  _mul,
+  _div,
+  _mod,
+  _and,
+  _or,
+  _xor,
+  _shl,
+  _shr,
+  _neg,
+  _not,
+};
 
 static op32 *read_op32(vm *_vm, inst *_inst) {
   op32 *_op32 = malloc(sizeof(op32));
@@ -66,10 +89,11 @@ static op64 *read_op64(vm *_vm, inst *_inst) {
   return _op64;
 }
 
-static vm *exec_add(vm *_vm, inst *_inst) {
+static vm *exec_arith(vm *_vm, inst *_inst,
+    uint64_t (*func)(uint64_t, uint64_t)) {
   if (_inst->is_64op) {
     op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 + _op64->op2;
+    uint64_t op = (uint64_t)func(_op64->op1, _op64->op2);
 
     if (_inst->op1_indirect)
       write_mem64(_vm->mem, _op64->op1val, op);
@@ -78,271 +102,7 @@ static vm *exec_add(vm *_vm, inst *_inst) {
     free(_op64);
   } else {
     op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 + _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_sub(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 - _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 - _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_mul(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 * _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 * _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_div(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 / _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 / _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_mod(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 % _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 % _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_and(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 & _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 & _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_or(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 | _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 | _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_xor(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 ^ _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 ^ _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_shl(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 << _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 << _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_shr(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = _op64->op1 >> _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = _op32->op1 >> _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_neg(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = -1 * _op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = -1 * _op32->op2;
-
-    if (_inst->op1_indirect)
-      write_mem32(_vm->mem, _op32->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = (uint64_t)0x00 << 32 & op;
-    free(_op32);
-  }
-
-  return _vm;
-}
-
-static vm *exec_not(vm *_vm, inst *_inst) {
-  if (_inst->is_64op) {
-    op64 *_op64 = read_op64(_vm, _inst);
-    uint64_t op = ~_op64->op2;
-
-    if (_inst->op1_indirect)
-      write_mem64(_vm->mem, _op64->op1val, op);
-    else
-      _vm->regs->regs[_inst->operand1] = op;
-    free(_op64);
-  } else {
-    op32 *_op32 = read_op32(_vm, _inst);
-    uint32_t op  = ~_op32->op2;
+    uint32_t op = (uint32_t)func(_op32->op1, _op32->op2);
 
     if (_inst->op1_indirect)
       write_mem32(_vm->mem, _op32->op1val, op);
@@ -367,51 +127,18 @@ static vm *inc_ip(vm *_vm) {
 vm *exec_op(vm *_vm, inst *_inst) {
   switch (_inst->opcode) {
     case ADD:
-      exec_add(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case SUB:
-      exec_sub(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case MUL:
-      exec_mul(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case DIV:
-      exec_div(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case MOD:
-      exec_mod(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case AND:
-      exec_and(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case OR:
-      exec_or(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case XOR:
-      exec_xor(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case SHL:
-      exec_shl(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case SHR:
-      exec_shr(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case NEG:
-      exec_neg(_vm, _inst);
-      inc_ip(_vm);
-      break;
     case NOT:
-      exec_not(_vm, _inst);
+      exec_arith(_vm, _inst, arith_ops[_inst->opcode]);
       inc_ip(_vm);
       break;
     default:
