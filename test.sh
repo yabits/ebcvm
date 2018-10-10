d8d28acf6d371020fd2e968b9882fc2c066582cd
Akira Moroo <retrage01@gmail.com>
2018-10-10 14:42:20 +0900

    Fix test.sh to exit on fail

--- test.sh
+++ test.sh
@@ -5,6 +5,11 @@ for file in $(ls test); do
   objs=`ls *.o | grep -v main.o`
   cc -o test/${test}.exe ${objs} test/${test}.o
   ./test/${test}.exe
+  out=$?
+  if [ "${out}" != 0 ]; then
+    exit 1
+  fi
+  echo "${test}: OK"
 done
 
 echo OK
