a17a13cfb0245f735b191cdf982c68e1cf4f8a77
Akira Moroo <retrage01@gmail.com>
2018-10-13 00:45:48 +0900

    Split decode_test into decode_*

--- test/decode_break_test.c
+++ test/decode_break_test.c
@@ -1,20 +1,23 @@
 #include <assert.h>
 #include "ebcvm.h"
 
-void break_test(void);
-
-void break_test() {
+static void break_test() {
   /* BREAK */
+  uint8_t *op = malloc(sizeof(uint8_t) * 2);
+  inst *_inst;
   for (int i = 0; i < 7; i++) {
-    uint8_t *op = malloc(sizeof(uint8_t) * 2);
     op[0] = 0x00;
     op[1] = 0x00 + i;
-    inst *_inst = decode_op(op);
+    _inst = decode_op(op);
     assert(_inst->opcode == BREAK);
     assert(_inst->break_code == i);
+    free(_inst);
   }
+  free(op);
 }
 
 int main() {
   break_test();
+
+  return 0;
 }
