a58a2011ed381fcadabe9cb258dd4f8025304b5b
Akira Moroo <retrage01@gmail.com>
2018-11-20 18:42:54 +0900

    Change to realloc before loading sections

--- load.c
+++ load.c
@@ -38,17 +38,12 @@ static vm *do_load_exe(const char *addr, vm *_vm) {
   if (!_vm->memmap)
     goto fail;
 
+  /* enumerate sections */
   IMAGE_SECTION_HEADER *sechdr;
   for (int i = 0; i < fhdr->NumberOfSections; i++) {
     sechdr = (IMAGE_SECTION_HEADER *)
       (addr + doshdr->e_lfanew
        + sizeof(IMAGE_NT_HEADERS) + sizeof(IMAGE_SECTION_HEADER) * i);
-    if (image_base + sechdr->VirtualAddress > _vm->mem->size)
-      error("out of memory");
-    void *dst = _vm->mem->body + image_base + sechdr->VirtualAddress;
-    void *src = (void *)addr + sechdr->PointerToRawData;
-    size_t n = sechdr->Misc.VirtualSize;
-    memcpy(dst, src, n);
     if (!memcmp(sechdr->Name, ".text", 5))
       _vm->memmap[i].mem_type = MEM_TEXT;
     else if (!memcmp(sechdr->Name, ".data", 5))
@@ -61,15 +56,35 @@ static vm *do_load_exe(const char *addr, vm *_vm) {
     _vm->memmap[i].size = sechdr->Misc.VirtualSize;
   }
 
+  uint32_t align = opthdr->SectionAlignment;
+  uint16_t last = _vm->memmap_size - 1;
+  uint64_t tail = _vm->memmap[last].addr + _vm->memmap[last].size;
+  /* realloc memory if AUTO_MEM_SIZE and mem size if too small */
+  if (!FLAGS_mem && _vm->mem->size < tail) {
+    /* XXX: tail + EFI section */
+    size_t size = ((tail / align) + 2) * align;
+    _vm->mem = realloc_mem(_vm->mem, size);
+  }
+
+  /* load sections */
+  for (int i = 0; i < fhdr->NumberOfSections; i++) {
+    sechdr = (IMAGE_SECTION_HEADER *)
+      (addr + doshdr->e_lfanew
+       + sizeof(IMAGE_NT_HEADERS) + sizeof(IMAGE_SECTION_HEADER) * i);
+    if (image_base + sechdr->VirtualAddress > _vm->mem->size)
+      error("out of memory");
+    void *dst = _vm->mem->body + image_base + sechdr->VirtualAddress;
+    void *src = (void *)addr + sechdr->PointerToRawData;
+    size_t n = sechdr->Misc.VirtualSize;
+    memcpy(dst, src, n);
+  }
+
   /* setup registers */
   _vm->regs->regs[IP] = image_base + entry_point;
   _vm->regs->regs[R0] = STACK_BASE; /* FIXME: stack pointer */
   write_mem64(_vm->mem, STACK_BASE, STACK_MAGIC);
 
   /* calculate efi entry point address */
-  uint32_t align = opthdr->SectionAlignment;
-  uint16_t last = _vm->memmap_size - 1;
-  uint64_t tail = _vm->memmap[last].addr + _vm->memmap[last].size;
   uint64_t efi_addr = ((tail / align) + 1 ) * align;
 
   _vm = load_efi(efi_addr, _vm);
