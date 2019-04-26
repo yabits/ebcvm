1b2438632ccb3a0e79e30b08c93162395263a6ba
Akira Moroo <retrage01@gmail.com>
2019-04-26 15:06:36 +0900

    Fix exec_storesp_test
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- test/exec_storesp_test.c
+++ test/exec_storesp_test.c
@@ -5,14 +5,21 @@ static void storesp_test() {
   vm *_vm;
   inst *_inst = malloc(sizeof(inst));
   _inst->opcode = STORESP;
-  for (_inst->operand2 = IP; _inst->operand2 <= RV7; _inst->operand2++) {
-    for (_inst->operand1 = R0; _inst->operand1 <= R7; _inst->operand1++) {
-      _vm = init_vm();
-      _vm->regs->regs[_inst->operand2] = 0x0123456789abcdef;
-      _vm = exec_op(_vm, _inst);
-      assert(_vm->regs->regs[_inst->operand1] == 0x0123456789abcdef);
-      fini_vm(_vm);
-    }
+  _inst->operand2 = FLAGS;
+  for (_inst->operand1 = R0; _inst->operand1 <= R7; _inst->operand1++) {
+    _vm = init_vm();
+    _vm->regs->regs[_inst->operand2] = 0x0123456789abcdef;
+    _vm = exec_op(_vm, _inst);
+    assert(_vm->regs->regs[_inst->operand1] == 0x0123456789abcdef);
+    fini_vm(_vm);
+  }
+  _inst->operand2 = IP;
+  for (_inst->operand1 = R0; _inst->operand1 <= R7; _inst->operand1++) {
+    _vm = init_vm();
+    _vm->regs->regs[_inst->operand2] = 0x0123456789abcdef;
+    _vm = exec_op(_vm, _inst);
+    assert(_vm->regs->regs[_inst->operand1] == 0x0123456789abcdf1);
+    fini_vm(_vm);
   }
 }
 
